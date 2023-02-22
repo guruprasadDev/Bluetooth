a4d39fb85aafbaec5dd4f33118be8df5e24153a2
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-02-22 11:35:47 +0530

    permission  helper class

---
+++ app/src/main/java/com/guru/bluetooth/helper/PermissionsHelper.kt
@@ -0,0 +1,31 @@
+package com.guru.bluetooth.helper
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.guru.bluetooth.utils.Constants.REQUEST_ACCESS_COARSE_LOCATION
+import com.guru.bluetooth.utils.Constants.REQUEST_READ_EXTERNAL_STORAGE
+
+class PermissionsHelper(private val activity: Activity) {
+
+    fun requestAccessCoarseLocationPermissionIfNotGranted() {
+        val permission = android.Manifest.permission.ACCESS_COARSE_LOCATION
+        requestPermissionIfNotGranted(permission, REQUEST_ACCESS_COARSE_LOCATION)
+    }
+
+    fun requestReadExternalStoragePermissionIfNotGranted() {
+        val permission = android.Manifest.permission.READ_EXTERNAL_STORAGE
+        requestPermissionIfNotGranted(permission, REQUEST_READ_EXTERNAL_STORAGE)
+    }
+
+    private fun requestPermissionIfNotGranted(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(activity, permission)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                ActivityCompat.requestPermissions(activity, arrayOf(permission), requestCode)
+            }
+        }
+    }
+}
