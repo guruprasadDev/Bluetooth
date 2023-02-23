7cfad9f8024033db10785d1f3a6a002d576e9345
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-02-23 11:40:19 +0530

    Add BluetoothClient class for Android application to send files via Bluetooth

--- app/src/main/java/com/guru/bluetooth/client/BluetoothClient.kt
+++ app/src/main/java/com/guru/bluetooth/client/BluetoothClient.kt
@@ -1,7 +1,12 @@
 package com.guru.bluetooth.client
 
-import android.annotation.SuppressLint
+import android.Manifest
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.guru.bluetooth.BluetoothConnectionService
 import java.io.BufferedInputStream
 import java.io.File
@@ -9,21 +14,46 @@ import java.io.FileInputStream
 import java.io.IOException
 import java.nio.ByteBuffer
 
-class BluetoothClient(device: BluetoothDevice) : Thread() {
+class BluetoothClient(private val context: Context, device: BluetoothDevice) : Thread() {
 
-    @SuppressLint("MissingPermission")
-    private val socket = device.createRfcommSocketToServiceRecord(BluetoothConnectionService.uuid)
+    private val socket: BluetoothSocket?
 
-    @SuppressLint("MissingPermission")
+    init {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.BLUETOOTH
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.BLUETOOTH_ADMIN
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            socket = device.createRfcommSocketToServiceRecord(BluetoothConnectionService.uuid)
+        } else {
+            socket = null
+        }
+    }
+
     override fun run() {
+        if (socket == null) {
+            return
+        }
+
         try {
-            this.socket.connect()
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            socket.connect()
         } catch (e: IOException) {
             return
         }
 
-        val outputStream = this.socket.outputStream
-        val inputStream = this.socket.inputStream
+        val outputStream = socket.outputStream
+        val inputStream = socket.inputStream
         val file = File(BluetoothConnectionService.fileURI)
         val fileBytes: ByteArray
         try {
@@ -41,16 +71,17 @@ class BluetoothClient(device: BluetoothDevice) : Thread() {
         val fileSize = ByteBuffer.allocate(4)
         fileSize.putInt(fileBytes.size)
 
-        outputStream.write(fileNameSize.array())
-        outputStream.write(file.name.toByteArray())
-        outputStream.write(fileSize.array())
-        outputStream.write(fileBytes)
+        outputStream.apply {
+            write(fileNameSize.array())
+            write(file.name.toByteArray())
+            write(fileSize.array())
+            write(fileBytes)
+        }
 
-        BluetoothConnectionService.isSuccess = true
-
+        BluetoothConnectionService.isFileTransferSuccessful = true
         sleep(5000)
         outputStream.close()
         inputStream.close()
-        this.socket.close()
+        socket.close()
     }
 }
