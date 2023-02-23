bbdde4513dec8fda4e473a5d9ce089e5f32c5393
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-02-23 11:43:11 +0530

    add 2 more constants

--- app/src/main/java/com/guru/bluetooth/utils/Constants.kt
+++ app/src/main/java/com/guru/bluetooth/utils/Constants.kt
@@ -3,4 +3,6 @@ package com.guru.bluetooth.utils
 object Constants {
     const val REQUEST_ACCESS_COARSE_LOCATION = 100
     const val REQUEST_READ_EXTERNAL_STORAGE = 200
+    const val REQUEST_ENABLE_BT = 1
+    const val EXTRA_MESSAGE = "com.example.app.EXTRA_MESSAGE"
 }
